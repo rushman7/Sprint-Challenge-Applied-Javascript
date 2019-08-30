83ccd10ffcd0216eeecb69d5a8f752df8295da67
BuildTools <arthurpisakhov1@gmail.com>
2019-08-30 00:51:21 -0400

    header/index.js

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,28 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+function Header() {
+  let header = document.createElement('div');
+  header.classList.add('header');
+
+  let date = document.createElement('span');
+  date.classList.add('date');
+  date.textContent = 'MARCH 28, 2019';
+  header.appendChild(date);
+
+  let h1 = document.createElement('h1');
+  h1.textContent = 'Lambda Times';
+  header.appendChild(h1);
+
+  let temp = document.createElement('span');
+  temp.classList.add('temp');
+  temp.textContent = '98°';
+  header.appendChild(temp);
+
+  console.log(header);
+  return header;
+}
+
+const container = document.querySelector('.header-container')
+
+container.appendChild(Header())
